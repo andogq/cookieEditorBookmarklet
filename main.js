974d56a2ce59f476a6247e26655955d96581e908
Tom Anderson <portlester.tom@gmail.com>
2017-01-18 16:51:20 +1100

    Creates window

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-alert("hello");
+function makeWindow() {
+    var holder = document.createExpression("div");
+    holder.innerHTML = "<div id=\"cookieWindow\"></div>";
+    var window = holder.firstChild();
+    document.body.append(window);
+}
